4960326904d880d4919db36e187dea820e9a95fa
gogomorgan <macrimorganb@gmail.com>
2018-04-13 12:18:14 -0400

    Files

---
+++ Assets/PlayerControl.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    public float speed = 6f;
+    public float jumpspeed = 2f;
+    Rigidbody rb;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        Debug.Log(moveHorizontal);
+        float moveVertical = Input.GetAxis("Vertical");
+        Debug.Log(moveVertical);
+
+        Vector3 movement = new Vector3(moveHorizontal*speed, rb.velocity.y, moveVertical*speed);
+        rb.velocity = movement;    
+
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(Vector3.up * jumpspeed);
+        }
+    }
+}
